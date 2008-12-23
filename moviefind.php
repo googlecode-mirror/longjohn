ccac6ad1f070779a39d3595ca24f46fec07951c3
kevinbatchelor <kevinbatchelor@e7d0a968-cb8f-11dd-8772-d1b041bf0e37>
2008-12-23 00:57:57 +0000

    fixed 4 column layout

--- moviefind.php
+++ moviefind.php
@@ -23,23 +23,22 @@ if(!isset($cmd))
    $result = mysql_query("SELECT * FROM titles WHERE titles.name LIKE '%".$_POST['search1']."%'AND 
    titles.catagory LIKE'".$_POST['category']."'order by titles.name");
 $counter = 0;
-echo "<table width=600>";
+echo "<table width=600 border = 1>";
 echo "<tr>";
    while($row=mysql_fetch_array($result))
    {
    $movID=$row['0'];
-	    
-        //echo "<td>";
-		echo "<td align=center>";
-		echo "<a href='divx.php?play=".$row['name']."'>";
-		echo "<img src=pics/".$row['name'].'.jpg'."  alt='".$row['name']."'></td>";
-		echo "</a>";
-		//echo "<a href='divxSmall.php?play=".$row['name']."'>"." / Small"."</a><br>";
-		//echo $row['category']. "<br>";
+		$movName= $row['name'];
+	    echo "<td align=center>";
+		echo "<a href='divx.php?play=$movName' title=$movName>";
+		echo "<img src='pics/$movName.jpg'>"; 
+		echo "<br>";
+		echo substr($movName,0,10);
+		echo "...";
 		
-        
-		//echo "<img src=pics/".$row['name'].'.jpg'." ></td>";
-	    echo "</td>";
+		echo "</a>";
+		echo "</td>";
+		
 		$counter += 1;
       if ( $counter % 4 == 0 ) { echo "</tr><tr>"; }
      }
