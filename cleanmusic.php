733b6f82ec056d3df1e3f71da08a1c9ce1ef5007
kevinbatchelor <kevinbatchelor@e7d0a968-cb8f-11dd-8772-d1b041bf0e37>
2009-12-15 19:30:19 +0000

    added exceptions to clean file

--- cleanmusic.php
+++ cleanmusic.php
@@ -7,6 +7,7 @@ $result = mysql_query("delete from music where name like '%.txt'");
 $result = mysql_query("delete from music where name like '%.torrent'");
 $result = mysql_query("delete from music where name like '%.pdf'");
 $result = mysql_query("delete from music where name like '%.db'");
+$result = mysql_query("delete from music where name like '%.alb'");
 echo "Non music files removed."
 ?>
 <? require_once "footer.php";?>
