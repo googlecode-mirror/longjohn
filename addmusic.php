63300b101c0bc8e47569bcf3f5ccdf489e2d3b6b
kevinbatchelor <kevinbatchelor@e7d0a968-cb8f-11dd-8772-d1b041bf0e37>
2009-12-15 19:31:16 +0000

    these files contain the version from work where the path to the file is in htdocs

--- addmusic.php
+++ addmusic.php
@@ -1,7 +1,8 @@
 <?require_once "header.php";?>
 
 <?php
-$it = new RecursiveDirectoryIterator("e:/mp3s");
+//replace mp3s with the path to your music folder, if your music folder is not in your htdocs, then pass in the absolute path.
+$it = new RecursiveDirectoryIterator("mp3s");
 foreach(new RecursiveIteratorIterator($it) as $file) {
 	$name = $file->getFilename();
 	$name = htmlspecialchars($name,ENT_QUOTES);
@@ -11,7 +12,7 @@ foreach(new RecursiveIteratorIterator($it) as $file) {
 	$folder=prev($folderarray);
 	$path=str_replace("\\","/",$file);
 	//uncomment the next line if you pass an absolute path to the Iterator, you will need to give apache access to the root of that directory if you use this
-	$path=substr($path,3);
+	//$path=substr($path,3);
 	$sql="SELECT id FROM music WHERE music.category ='All' AND music.path = '".$path."'";	
 	$res = mysql_query($sql) or die ("SQL failed");
 	
